a8c6e67829ef993278b97bb6b4f60040d34de6f0
skivel <denisbuhrov@gmail.com>
2022-04-08 15:53:54 +0300

    Add shop template && optimisation templates blocks

--- gun_shop/main/views.py
+++ gun_shop/main/views.py
@@ -5,5 +5,9 @@ def index(request):
     return render(request, 'main/index.html')
 
 
+def shop(request):
+    return render(request, 'main/shop.html')
+
+
 def about(request):
     return render(request, 'main/about.html')
