e4f8e949e7abcaf94f2ea328e3fdef38585888ac
skivel <denisbuhrov@gmail.com>
2022-04-12 09:42:28 +0200

    add Shop migrations

--- gun_shop/shop/models.py
+++ gun_shop/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField('Назва категорії', max_length=255)
+    slug = models.SlugField(max_length=55)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
