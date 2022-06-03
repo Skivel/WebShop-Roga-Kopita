3174f572c35c3bf6448c3bcb3ba7fcb6ee61d2c2
skivel <denisbuhrov@gmail.com>
2022-06-03 13:00:58 +0200

    save; shop.start(JS);

--- gun_shop/main/urls.py
+++ gun_shop/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from gun_shop.main import views
 
 urlpatterns = [
     path('', views.index),
--- gun_shop/main/views.py
+++ gun_shop/main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from gun_shop.shop.models import Products
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    products = Products.objects.all()
+    return render(request, 'main/index.html', {'products': products})
 
 
 def about(request):
